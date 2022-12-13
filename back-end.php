750087bfd2cdef15301466d6d0daa5bb1d5e9c6d
Crithiano Cunha <cristhiano.cunha@gmail.com>
2022-12-13 17:02:28 -0300

    configurando back-end incio

---
+++ back-end.php
@@ -0,0 +1,20 @@
+<?php
+    if(isset($_POST['submit']))
+    {
+        print_r($_POST['nome']);
+        print_r($_POST['data_nascimento']);
+        print_r($_POST['email']);
+        print_r($_POST['telefone']);
+        print_r($_POST['regiao']);
+        
+        
+
+
+
+
+
+    }
+
+
+
+?>
