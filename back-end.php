c58b610b1e02f61e80f51bcfafab4ae013508eab
Crithiano Cunha <cristhiano.cunha@gmail.com>
2022-12-14 23:00:33 -0300

    criado conexao db

--- back-end.php
+++ back-end.php
@@ -1,20 +1,7 @@
 <?php
-    if(isset($_POST['submit']))
-    {
-        print_r($_POST['nome']);
-        print_r($_POST['data_nascimento']);
-        print_r($_POST['email']);
-        print_r($_POST['telefone']);
-        print_r($_POST['regiao']);
-        
-        
-
-
-
-
-
-    }
-
-
+    
+    $nome = isset($_POST["nome"])?$_POST["nome"]:"[nao informado]";
+    echo "$nome";
+    
 
 ?>
