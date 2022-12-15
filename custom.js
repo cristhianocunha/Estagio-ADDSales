ce9f1c71d03f5724d6b6e8492f1020912562a980
Crithiano Cunha <cristhiano.cunha@gmail.com>
2022-12-15 16:40:03 -0300

    criando um ajax

--- custom.js
+++ custom.js
@@ -1,31 +1,30 @@
-let nome = document.getElementsByName("nome");
-let telefone = document.getElementsByName("telefone");
-let email = document.getElementsByName("email");
-let data_nascimento = document.getElementsByName("data_nascimento");
-let regiao = document.getElementsByName("regiao");
-                
-                var formCadUsuario = [nome, telefone, email, data_nascimento, regiao];
-                
-                    
-                    
-                   
-                    console.log(formCadUsuario);
-                    
+$(function () {
+    $('.next-step').click(function (event) {
+        event.preventDefault();
+        $(this).parents('.form-step').hide().next().show();
+        
+    });
+});
 
 
-
-                    if (formCadUsuario) {
-                        
-                            // Receber os dados do formulario
-                            const dadosForm = new FormData(formCadUsuario);
-                    
-                            // Enviar os dados para o arquivo "cadastrar.php", deve salvar no BD
-                            const dados = await fetch("back-end.php", {
-                                method: "POST",
-                                body: dadosForm
-                            });
-                            const resposta = await dados.json();
-                            //console.log(resposta);
-                    
-                            
-                        };
+$(document).ready(function () {
+    $("form").click(function (event) {
+      var formData = {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        superheroAlias: $("#telefone").val(),
+      };
+  
+      $.ajax({
+        type: "POST",
+        url: "backend.php",
+        data: formData,
+        dataType: "json",
+        encode: true,
+      }).done(function (data) {
+        console.log(data);
+      });
+  
+      event.preventDefault();
+    });
+  });
