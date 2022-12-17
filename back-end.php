8984e60f9a030b897af7ff8b007992acd3497eab
Crithiano Cunha <cristhiano.cunha@gmail.com>
2022-12-17 17:05:04 -0300

    resolvendo conexão ajax com back-end e formato data_nascimento

--- back-end.php
+++ back-end.php
@@ -1,24 +1,42 @@
 <?php
 
 // Include a conexao BD
+
+
+
+// echo json_encode($retorna);
+
+
+
+/*
+* 
+* capturando dados do fron-end form
+*/
+// STR_TO_DATE(str,format)
+
+// POST Data
+$data['nome'] = $_POST['nome'];
+$data['data_nascimento'] = $_POST['data_nascimento'];
+$data['email'] = $_POST['email'];
+$data['telefone'] = $_POST['telefone'];
+$data['regiao'] = $_POST['regiao'];
+$data['unidade'] = $_POST['unidade'];
+ 
+// return json_encode($data);
+echo json_encode($data);
 include_once "db.php";
 
-// Receber os dados enviado pelo JavaScript
-$dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+$data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-if(empty($dados['nome'])){
-    $retorna = ['status' => false, 'msg' => "<p style='color: #f00;'>Erro: Necessário preencher o campo nome!</p>"];
-}elseif(empty($dados['email'])){
-    $retorna = ['status' => false, 'msg' => "<p style='color: #f00;'>Erro: Necessário preencher o campo e-mail!</p>"];
-}elseif(empty($dados['data_nascimento'])){
-    $retorna = ['status' => false, 'msg' => "<p style='color: #f00;'>Erro: Necessário preencher o campo situação!</p>"];
-}else{
 
-    $query_usuario = "INSERT INTO leads (nome, email, data_nascimento) VALUES (:nome, :email, :data_nascimento)";
+    $query_usuario = "INSERT INTO leads (nome, data_nascimento, email, telefone, regiao, unidade) VALUES (:nome, :data_nascimento, :email, :telefone, :regiao, :unidade )";
     $cad_usuario = $conn->prepare($query_usuario);
-    $cad_usuario->bindParam(':nome', $dados['nome']);
-    $cad_usuario->bindParam(':email', $dados['email']);
-    $cad_usuario->bindParam(':data_nascimento', $dados['data_nascimento']);
+    $cad_usuario->bindParam(':nome', $data['nome']);
+    $cad_usuario->bindParam(':data_nascimento', $data['data_nascimento']);
+    $cad_usuario->bindParam(':email', $data['email']);
+    $cad_usuario->bindParam(':telefone', $data['telefone']);
+    $cad_usuario->bindParam(':regiao', $data['regiao']);
+    $cad_usuario->bindParam(':unidade', $data['unidade']);
     $cad_usuario->execute();
 
     if($cad_usuario->rowCount()){
@@ -26,5 +44,3 @@ if(empty($dados['nome'])){
     }else{
         $retorna = ['status' => false, 'msg' => "<p style='color: #f00;'>Erro: Usuário não cadastrado com sucesso!</p>"];
     }    
-}
-echo json_encode($retorna);
