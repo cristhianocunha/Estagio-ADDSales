9f923ebac2706e8f07a6d9e195d0113664f06bc7
Crithiano Cunha <cristhiano.cunha@gmail.com>
2022-12-18 01:18:57 -0300

    Regiao likando com unidede e calculos dos ponto de regiao

--- back-end.php
+++ back-end.php
@@ -1,18 +1,10 @@
 <?php
 
-// Include a conexao BD
-
-
-
-// echo json_encode($retorna);
-
-
-
 /*
 * 
 * capturando dados do fron-end form
 */
-// STR_TO_DATE(str,format)
+
 
 // POST Data
 $data['nome'] = $_POST['nome'];
@@ -21,7 +13,54 @@ $data['email'] = $_POST['email'];
 $data['telefone'] = $_POST['telefone'];
 $data['regiao'] = $_POST['regiao'];
 $data['unidade'] = $_POST['unidade'];
- 
+
+// variaveis
+
+$x = $data['regiao'];
+$dataNasc = $data['data_nascimento'];
+$ponts = 10;
+$idade = 0;
+// calculando pontos da idade
+$data = new DateTime($dataNasc);
+$idade = $data->diff( new DateTime(date('Y-m-d')));
+
+$idade = $idade->format('Y');
+
+
+if ($idade < 18 ){
+    $ponts -= 5;
+}elseif($idade >= 40  && $idade <= 99) {
+    $ponts -= 3;
+}elseif($idade <= 100){
+    $ponts -= 5;
+}
+
+
+
+
+// calculando pontuação da regiao
+
+
+
+switch ($x){
+    case "sul":
+        $ponts -= 4;
+        break;
+    case "sude":
+        $ponts -= 1;
+        break;
+    case "cent":
+        $ponts -= 3;
+        break;
+    case "nord":
+        $ponts -= 2;
+        break;
+    case "nort":
+        $ponts -= 5;
+        
+}
+
+
 // return json_encode($data);
 echo json_encode($data);
 include_once "db.php";
@@ -29,7 +68,7 @@ include_once "db.php";
 $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 
-    $query_usuario = "INSERT INTO leads (nome, data_nascimento, email, telefone, regiao, unidade) VALUES (:nome, :data_nascimento, :email, :telefone, :regiao, :unidade )";
+    $query_usuario = "INSERT INTO leads (nome, data_nascimento, email, telefone, regiao, unidade, ponts) VALUES (:nome, :data_nascimento, :email, :telefone, :regiao, :unidade, :ponts )";
     $cad_usuario = $conn->prepare($query_usuario);
     $cad_usuario->bindParam(':nome', $data['nome']);
     $cad_usuario->bindParam(':data_nascimento', $data['data_nascimento']);
@@ -37,6 +76,7 @@ $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
     $cad_usuario->bindParam(':telefone', $data['telefone']);
     $cad_usuario->bindParam(':regiao', $data['regiao']);
     $cad_usuario->bindParam(':unidade', $data['unidade']);
+    $cad_usuario->bindParam(':ponts', $ponts);
     $cad_usuario->execute();
 
     if($cad_usuario->rowCount()){
--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function() {
  console.log('primeiro');
     $("#submit").click(function() {
